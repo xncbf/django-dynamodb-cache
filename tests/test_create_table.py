9811c68d0b70de2d01686ef4fc9d0e0935e71754
xncbf <xncbf12@gmail.com>
2022-03-19 14:41:31 +0900

    update: table name

--- tests/test_create_table.py
+++ tests/test_create_table.py
@@ -9,10 +9,8 @@ from django_dynamodb_cache.settings import Settings
 @mock_dynamodb2
 class TestCreateTable(TestCase):
     def test_create_table_simple(self):
-
-        settings = Settings(aws_region_name="us-east-1", table_name="test_django_dynamodb_cache")
+        settings = Settings(aws_region_name="us-east-1", table_name="test-django-dynamodb-cache")
         dynamodb = get_dynamodb(settings)
-
         table = create_table(settings, dynamodb)
-        self.assertEqual(table.table_name, "test_django_dynamodb_cache")
+        self.assertEqual(table.table_name, "test-django-dynamodb-cache")
         self.assertEqual(table.table_status, "ACTIVE")
