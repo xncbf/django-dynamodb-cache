685c659c1a89e8238760c55b1ea37dd040be86f2
xncbf <xncbf12@gmail.com>
2022-03-21 10:57:47 +0900

    ADD: test

--- tests/test_cache_simple.py
+++ tests/test_cache_simple.py
@@ -24,27 +24,31 @@ class TestCacheSimple(TestCase):
         super().tearDownClass()
 
     def test_set_simple(self):
-        self.cache.set("test-django-dynamodb-cache", "too much love")
-        item = self.cache.get("test-django-dynamodb-cache")
-        self.assertEqual(item, "too much love")
+        self.cache.set("set_simple", "test")
+        item = self.cache.get("set_simple")
+        self.assertEqual(item, "test")
 
-        self.cache.delete("test-django-dynamodb-cache")
-        value = self.cache.get("test-django-dynamodb-cache", 1001)
+        self.cache.set("set_simple", "test2")
+        item = self.cache.get("set_simple")
+        self.assertEqual(item, "test2")
+
+        self.cache.delete("set_simple")
+        value = self.cache.get("set_simple", 1001)
         self.assertEqual(value, 1001)
 
     def test_get_delete_many(self):
-        items = {f"test-django-dynamodb-cache_{i}": f"too much love {i}" for i in range(10)}
+        items = {f"get_delete_many_{i}": f"test {i}" for i in range(10)}
 
         self.cache.set_many(items)
         from_cache = self.cache.get_many(items.keys())
         self.assertEqual(items, from_cache)
         self.cache.delete_many(items.keys())
 
-        value = self.cache.get("test-django-dynamodb-cache_1", 1001)
+        value = self.cache.get("get_delete_many_1", 1001)
         self.assertEqual(value, 1001)
 
     def test_add(self):
-        self.cache.set("test_add", "some set")
+        self.cache.add("test_add", "some set")
         self.cache.add("test_add", "another add")
         value = self.cache.get("test_add", "default")
         self.assertEqual(value, "some set")
@@ -52,7 +56,6 @@ class TestCacheSimple(TestCase):
     def test_expired(self):
         self.cache.set("expired", "lost data", -1000)
         value = self.cache.get("expired", 1001)
-
         self.assertEqual(value, 1001)
 
     def test_incr_dec_version(self):
@@ -78,8 +81,25 @@ class TestCacheSimple(TestCase):
         mock_warn.assert_called()
 
     def test_incr_decr_value(self):
-
         self.cache.set("i", 10)
         self.cache.decr("i", 42)
         value = self.cache.get("i")
         self.assertEqual(value, -32)
+
+    def test_touch(self):
+        self.cache.touch("touch")
+        value = self.cache.get("touch")
+        self.assertEqual(value, None)
+
+    def test_get_or_set(self):
+        value = self.cache.get("get_or_set")
+        self.assertEqual(value, None)
+        value = self.cache.get_or_set("get_or_set", 10)
+        self.assertEqual(value, 10)
+        value = self.cache.get_or_set("get_or_set", 11)
+        self.assertEqual(value, 11)
+        self.cache.delete("get_or_set")
+        value = self.cache.get("get_or_set")
+        self.assertEqual(value, None)
+        value = self.cache.get_or_set("get_or_set", lambda: 12)
+        self.assertEqual(value, 12)
