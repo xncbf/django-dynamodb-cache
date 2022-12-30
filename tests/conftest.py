0e8834e856a7e6a1a2513d4ddfbdcec522cfa616
Joon Hwan 김준환 <xncbf12@gmail.com>
2022-12-30 22:38:49 +0900

    chore: add sandbox for dynamodb (#14)
    * chore: add sandbox
    
    * feat: mocking get_dynamodb
    
    * test: cov test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,20 @@
-from moto import mock_dynamodb
+import pytest
 
+from django_dynamodb_cache import Cache
 from django_dynamodb_cache.dynamodb import create_table, get_dynamodb
 from django_dynamodb_cache.settings import Settings
-from tests.conf import TABLE_NAME
+
+from .conf import TABLE_NAME
 
 
-@mock_dynamodb
-def test_create_table_simple():
+@pytest.fixture(scope="session")
+def cache():
     settings = Settings(aws_region_name="us-east-1", table_name=TABLE_NAME)
     dynamodb = get_dynamodb(settings)
+    cache = Cache(settings)
     table = create_table(settings, dynamodb)
-    assert table.table_name == TABLE_NAME
-    assert table.table_status == "ACTIVE"
+    yield cache
+    print("teardown")
+    table.delete(
+        TableName=settings.table_name,
+    )
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,21 +1,19 @@
-from unittest import TestCase
-
 from django_dynamodb_cache import Cache
 from django_dynamodb_cache.settings import Settings
 
 
-class TestSettings(TestCase):
-    def test_simples(self):
-        settings = Settings(version=10)
+def test_simples():
+    settings = Settings(version=10)
 
-        self.assertEqual(settings["version"], settings.version)
-        self.assertEqual(settings.get("version"), settings.version)
-        self.assertEqual(settings.version, 10)
+    assert settings["version"] == settings.version
+    assert settings.get("version") == settings.version
+    assert settings.version == 10
 
-    def test_encode(self):
-        settings = Settings(encode="json")
 
-        cache = Cache(settings)
+def test_encode():
+    settings = Settings(encode="json")
 
-        j = cache.encode.dumps({"a": 99})
-        self.assertEqual(j, '{"a": 99}')
+    cache = Cache(settings)
+
+    j = cache.encode.dumps({"a": 99})
+    assert j == '{"a": 99}'
