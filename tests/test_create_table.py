87acd1b750fd5e0a54e97b562244a44a61698e86
xncbf <xncbf12@gmail.com>
2023-08-24 20:23:12 +0900

    remove: mock dynamodb

--- tests/test_create_table.py
+++ tests/test_create_table.py
@@ -1,11 +1,9 @@
-from moto import mock_dynamodb
-
 from django_dynamodb_cache.dynamodb import create_table, get_dynamodb
 from django_dynamodb_cache.settings import Settings
 from tests.conf import TABLE_NAME
 
 
-@mock_dynamodb
+# @mock_dynamodb
 def test_create_table_simple():
     settings = Settings(aws_region_name="us-east-1", table_name=TABLE_NAME)
     dynamodb = get_dynamodb(settings)
